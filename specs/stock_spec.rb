0b76cd82d7aa2c48ea9b4970c7764bf7fb9c7172
A_Lippok <alippok@hotmail.co.uk>
2016-03-01 12:18:42 +0000

    Adds a tag parameter for stock

--- specs/stock_spec.rb
+++ specs/stock_spec.rb
@@ -5,10 +5,11 @@ require_relative('../stock.rb')
 class TestStock < MiniTest::Test
 
 def setup
-@stock1 = Stock.new(item_description: '4 large plates', price: 25, id:'LP4')
-@stock2 = Stock.new(item_description: '4 mugs', price: 5, id:'M4')
-@stock3 = Stock.new(item_description: '25 piece cutlery set', price: 15, id: 'CT25')
-@stock4 = Stock.new(item_description: '4 chrome table spoons', id:'TBSP4')
+@stock1 = Stock.new(item_description: '4 large plates', price: 25, id:'LP4', tag:'H')
+@stock2 = Stock.new(item_description: '4 mugs', price: 5, id:'M4', tag:'H')
+@stock3 = Stock.new(item_description: '25 piece cutlery set', price: 15, id: 'CT25', tag:'H')
+@stock4 = Stock.new(item_description: '4 chrome table spoons', id:'TBSP4', tag:'H')
+@stock5 = Stock.new(item_description: 'Lego City Fire Engine', id:'LCFE', tag:'T')
 
 end
 
@@ -18,6 +19,10 @@ end
 
 def test_if_stock_returns_id
   assert_equal('TBSP4', @stock4.id)
+end
+
+def test_if_stock_returns_tag
+  assert_equal('T', @stock5.tag)
 end
 
 
--- stock.rb
+++ stock.rb
@@ -1,11 +1,12 @@
 class Stock
 
-attr_reader(:price, :item_description, :id)
+attr_reader(:price, :item_description, :id, :tag)
 
 def initialize(params)
 @price = params[:price]
 @item_description = params[:item_description]
 @id = params[:id]
+@tag = params[:tag]
 
 end
 
