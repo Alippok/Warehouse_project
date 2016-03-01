94b9411d8c20434178cbb3a7c2f697b6ab5865e0
A_Lippok <alippok@hotmail.co.uk>
2016-03-01 13:00:19 +0000

    Adds method to lorry to show objects in hold

---
+++ lorry.rb
@@ -0,0 +1,15 @@
+class Lorry
+
+attr_reader(:hold)
+
+def initialize(hold)
+  @hold = hold
+
+end
+
+
+
+
+
+
+end
---
+++ specs/lorry_spec.rb
@@ -0,0 +1,28 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../lorry.rb')
+require_relative('../stock.rb')
+
+
+class TestLorry < MiniTest::Test
+
+def setup
+  @stock1 = Stock.new(item_description: '4 large plates', price: 25, id:'LP4', tag:'H')
+  @stock2 = Stock.new(item_description: '4 mugs', price: 5, id:'M4', tag:'H')
+  @stock3 = Stock.new(item_description: '25 piece cutlery set', price: 15, id: 'CT25', tag:'H')
+  @stock4 = Stock.new(item_description: '4 chrome table spoons', id:'TBSP4', tag:'H')
+  @stock5 = Stock.new(item_description: 'Lego City Fire Engine', id:'LCFE', tag:'T')
+
+  @stock_cache = [@stock1, @stock2, @stock3, @stock4, @stock5]
+
+@lorry1 = Lorry.new(@stock_cache)
+
+end
+
+def test_if_lorry_can_show_items_in_hold
+  assert_equal([@stock1, @stock2, @stock3, @stock4, @stock5], @lorry1.hold)
+end
+
+
+
+end
